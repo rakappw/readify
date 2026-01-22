83466212d611bc0c51b2b042273dcb2caf33dbb3
derryyy <dericwijaya90@gmail.com>
2026-01-22 14:22:10 +0700

    ahhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -276,7 +276,7 @@
 
         <div>
             <p>
-                halo deri, rakaaaaaaaaaaaaaaaaaaaa
+                halo deri, rakaaaaaaaaaaaaaaaaaaaa aaahhhhhhhh
             </p>
         </div>
     </body>
