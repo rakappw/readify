5ff235f54f9ac94950fb026d6b4a9173edf9fc2c
rakappw <r4.k4.ppw@gmail.com>
2026-01-22 14:05:54 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -271,7 +271,13 @@
         </div>
 
         @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
+            <div class="h-14.5 hidden lg:block"></div>\
         @endif
+
+        <div>
+            <p>
+                halo deri
+            </p>
+        </div>
     </body>
 </html>
